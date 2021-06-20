7c18067f4acebd42835841ebed3dc0b590618b8b
Kait <kweishaar@hawk.iit.edu>
2021-06-20 13:49:35 -0500

    for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,20 +10,54 @@
      //if incorrect: display wrong!, next question
 const timerEl = document.getElementById('countdown');
 const quizContainerEl = document.getElementById('quiz');
-const btnContainerEl = document.getElementById('button-container');
+//const btnContainerEl = document.getElementById('button-container');
 const descriptionEl = document.getElementById('description');
 const messageEl = document.getElementById('msg-display');
-const answersEl = document.getElementById('answers');
+//const feedbackEl = document.getElementById('feedback');
+const startbtnEl = document.getElementById('startbtn');
+
+
+var questions = [
+     {
+         question: 'Commonly used data types do NOT include:',
+         options: ["Strings", "Booleans", "Numbers", "Alerts"],
+         answerIdx: 3
+     },
+     {
+         question: 'Arrays in Javascript can be used to contain the following:',
+         options: ["Numbers and strings", "Booleans", "Arrays", "All of the above"],
+         answerIdx: 3
+     },
+     {
+         question:'The condition in an if/else statement is enclosed in ',
+         options:["Back ticks", "Quotation Marks", "Paranthesis", "Curly brackets"],
+         answerIdx: 2
+    },
+    {
+     question:'String values must be enclosed in _______ when being assigned to variables',
+     options:["Back ticks", "Quotation Marks", "Paranthesis", "Curly brackets"],
+     answerIdx: 1
+    },
+    {
+     question:'Iterating over a data set can be achieved through',
+     options:["for loop", "while loop", "setInterval", "All of the above"],
+     answerIdx: 3
+ },
+
+ ],
+ i,
+ 
+ numQuestions = 0,
+ answerDiv = document.getElementById('button-container'),
+ questionDiv = document.getElementById('msg-display'),
+ feedbackEl = document.getElementById('feedback');
 
 //Initial screen
 timerEl.textContent = '0';
 messageEl.textContent= 'Coding Quiz Challenge';
 descriptionEl.textContent = "Try to answer all of the questions within the time limit. Keep in mind an incorrect answer will deduct ten seconds from the remaining time!"
 
-var startButton = document.createElement("button");
-startButton.textContent = 'Start';
-startButton.className = "centered-button"
-btnContainerEl.appendChild(startButton);
+
 
 function countdown() {
   let timeLeft = 75;
@@ -39,7 +73,63 @@ function countdown() {
     }
   }, 1000);
 }
-startButton.onclick= function () {countdown()};
+startbtnEl.onclick= function () {
+     startGame()
+};
+
+const startGame = function() {
+     //hide button
+     startbtnEl.setAttribute('class', 'invisible');
+     descriptionEl.setAttribute('class', 'invisible');
+
+
+     countdown();
+     displayQuestion(questions.shift);
+}
+
+
+
+
+
+function displayQuestion(q) {  
+     // display the question itself
+     questionDiv.innerHTML = q.question;
+     feedbackEl.textContent= '';
+     // remove any existing buttons from answerDiv
+     answerDiv.innerHTML = '';
+     
+     // for each option in the 'options' array, create a button
+     for(i = 0; i < q.options.length; i++) {
+         btn = document.createElement('button');
+         btn.innerHTML = q.options[i];
+         btn.setAttribute('id',i);
+     
+         // create the click handler for the button
+         btn.onclick = function() {
+             var id = parseInt(this.getAttribute('id'),10);
+             
+             // if the id of the button matches the answer index,
+             // the user was right, so increment numRight and display it
+             if(id === q.answerIdx) {
+               feedbackEl.textContent= 'Correct!';
+             } else {
+               feedbackEl.textContent= 'Correct!';
+             }
+             
+             // if there are more questions to be displayed, 
+             // run the function again
+             if(questions.length) {
+                 displayQuestion(questions.shift()); 
+             } else {
+                 alert('Done!');
+             }                    
+         }
+             
+         // add the button to the answerDiv
+         answerDiv.appendChild(btn);        
+     }
+ }
+
 
   //Display end message and score
  // function displayEndMessage() {
